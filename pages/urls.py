c9fb52f804635f532237b079cb4a54b7ac405cd4
piet <pprotassow@posteo.de>
2025-01-08 16:17:23 +0100

    class-based & Templates

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import home_page_view
+from .views import home_page_view, HomePageView
 
 urlpatterns = [
+    path('', HomePageView.as_view(), name='my_home'),
     path('', home_page_view, name='my_home'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,12 @@
+from django.shortcuts import render
 from django.http import HttpResponse
+from django.views.generic import TemplateView
+
+class HomePageView(TemplateView):
+    template_name = "home.html" 
+
 
 def home_page_view(request):
+    return render(request, 'home.html')
     return HttpResponse("Hello World_bla!")
 
