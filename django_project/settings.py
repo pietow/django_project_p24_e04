7a0013d3964f7208b114d5c14cd1926f7a280035
piet <pprotassow@posteo.de>
2025-01-09 13:26:02 +0100

    extending templates & class based view

--- django_project/settings.py
+++ django_project/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'django_project.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -67,6 +67,7 @@ TEMPLATES = [
         },
     },
 ]
+print(BASE_DIR / "templates")
 
 WSGI_APPLICATION = 'django_project.wsgi.application'
 
--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,12 @@
-from django.http import HttpResponse
+from django.shortcuts import render
+from django.views.generic import TemplateView
 
 def home_page_view(request):
-    return HttpResponse("Hello World_bla!")
+    return render(request, "home.html")
+    
 
+class HomePageView(TemplateView):
+    template_name = "home.html"
+
+class AboutPageView(TemplateView):
+    template_name = "about.html"
